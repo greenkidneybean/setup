5fdb072c84008006735a95b8a418e8d7d7323f01
Michael Chambers <greenkidneybean@gmail.com>
2019-06-05 15:45:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@
 echo "Setting system preferences
 sh scripts/syst_prefs.sh
 
+# install applications view homebrew
+sh scripts/homebrew.sh
+
 # setup dock
 sh scripts/dock.sh
 
-# install applications view homebrew
-sh scripts/homebrew.sh
+# setup dotfiles
+sh scripts/dotfiles.sh
