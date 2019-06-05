e6c96e5d91bf74283aceb5ddb5db7365d41db7d0
Michael Chambers <greenkidneybean@gmail.com>
2019-06-05 15:32:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-echo "Install Homebrew packages"
-brew update
-brew tap Homebrew/bundle
-brew bundle
-brew cask cleanup
-
-# setup dock
-echo "Setting dock preferences
-sh dock.sh
-
 # run system preferences
 echo "Setting system preferences
 sh syst_prefs.sh
+
+# setup dock
+echo "Setting dock preferences"
+sh dock.sh
+
+# install applications view homebrew
+sh homebrew.sh
